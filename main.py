50ffe116b1e7575720da4862ecae918cb351c076
William Gardner <wg4568@gmail.com>
2019-10-21 20:11:04 -0700

    reordered

--- main.py
+++ main.py
@@ -24,10 +24,6 @@ def logout():
 def patients():
 	return 'unimplemented'
 
-@app.route('/patient/new')
-def patient_new():
-	return 'unimplemented'
-
 @app.route('/patient/<name>')
 def patient_detail(name):
 	return name
@@ -39,6 +35,10 @@ def patient_new_visit(name):
 @app.route('/patient/<name>/return_visit')
 def patient_return_visit(name):
 	return name
+
+@app.route('/patient/new')
+def patient_new():
+	return 'unimplemented'
 
 app.run(
 	debug=config['debug'],
