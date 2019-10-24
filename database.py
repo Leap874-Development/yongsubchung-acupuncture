c6046a95ff2b266192409199264942749ab5882b
William Gardner <wg4568@gmail.com>
2019-10-24 14:23:56 -0700

    indentation

--- database.py
+++ database.py
@@ -6,184 +6,184 @@ class DoctorExists(Exception): pass
 class PatientNotFound(Exception): pass
 
 def with_database(func):
-	def wrapped(self, *args, **kwargs):
-		conn = sqlite3.connect(self.path)
-		cur = conn.cursor()
-		resp = func(self, cur, *args, **kwargs)
-		conn.commit()
-		conn.close()
-		return resp
-	return wrapped
+    def wrapped(self, *args, **kwargs):
+        conn = sqlite3.connect(self.path)
+        cur = conn.cursor()
+        resp = func(self, cur, *args, **kwargs)
+        conn.commit()
+        conn.close()
+        return resp
+    return wrapped
 
 
 class Database:
-	def __init__(self, path, schema='documents/schema.sql'):
-		self.path = path
-		self.schema = open(schema, 'r').read()
-		
-		conn = sqlite3.connect(path)
-		cur = conn.cursor()
+    def __init__(self, path, schema='documents/schema.sql'):
+        self.path = path
+        self.schema = open(schema, 'r').read()
+        
+        conn = sqlite3.connect(path)
+        cur = conn.cursor()
 
-		try: cur.executescript(self.schema)
-		except sqlite3.OperationalError: pass
+        try: cur.executescript(self.schema)
+        except sqlite3.OperationalError: pass
 
-		conn.commit()
-		conn.close()
-	
-	@with_database
-	def doctor_exists(self, cur, username):
-		query = 'SELECT COUNT(*) FROM doctors WHERE username=?'
-		count = cur.execute(query, (username,)).fetchone()[0]
-		return bool(count)
+        conn.commit()
+        conn.close()
+    
+    @with_database
+    def doctor_exists(self, cur, username):
+        query = 'SELECT COUNT(*) FROM doctors WHERE username=?'
+        count = cur.execute(query, (username,)).fetchone()[0]
+        return bool(count)
 
-	@with_database
-	def doctor_add(self, cur, username, password):
-		if self.doctor_exists(username): raise DoctorExists()
-		hashed = security.generate_password_hash(password)
-		query = 'INSERT INTO doctors VALUES (?,?)'
-		cur.execute(query, (username, hashed))
+    @with_database
+    def doctor_add(self, cur, username, password):
+        if self.doctor_exists(username): raise DoctorExists()
+        hashed = security.generate_password_hash(password)
+        query = 'INSERT INTO doctors VALUES (?,?)'
+        cur.execute(query, (username, hashed))
 
-	@with_database
-	def doctor_check(self, cur, username, password):
-		if not self.doctor_exists(username): return False
-		query = 'SELECT * FROM doctors WHERE username=?'
-		doctor, hashed = cur.execute(query, (username,)).fetchone()
-		return security.check_password_hash(hashed, password)
+    @with_database
+    def doctor_check(self, cur, username, password):
+        if not self.doctor_exists(username): return False
+        query = 'SELECT * FROM doctors WHERE username=?'
+        doctor, hashed = cur.execute(query, (username,)).fetchone()
+        return security.check_password_hash(hashed, password)
 
-	# TODO: generate suffix
-	@with_database
-	def patient_add(self, cur, last_name, first_name, gender, dob,
-					height=None, weight=None, addr1=None, addr2=None,
-					phone1=None, phone2=None, email=None, medical_history=None,
-					medications=None, family_hx=None, allergy=None, note=None,
-					attachment=None):
+    # TODO: generate suffix
+    @with_database
+    def patient_add(self, cur, last_name, first_name, gender, dob,
+                    height=None, weight=None, addr1=None, addr2=None,
+                    phone1=None, phone2=None, email=None, medical_history=None,
+                    medications=None, family_hx=None, allergy=None, note=None,
+                    attachment=None):
 
-		suffix = 0
-		patient_key = last_name[0].upper() + dob.strftime('%y%m%d') + str(suffix)
-		create_date = datetime.date.today()
+        suffix = 0
+        patient_key = last_name[0].upper() + dob.strftime('%y%m%d') + str(suffix)
+        create_date = datetime.date.today()
 
-		query = 'INSERT INTO patient VALUES (%s)' % ('?,' * 20)[:-1]
-		cur.execute(query, (
-			create_date, suffix, patient_key, last_name, first_name, gender,
-			dob, height, weight, addr1, addr2, phone1, phone2, email,
-			medical_history, medications, family_hx, allergy, note, attachment
-		))
+        query = 'INSERT INTO patient VALUES (%s)' % ('?,' * 20)[:-1]
+        cur.execute(query, (
+            create_date, suffix, patient_key, last_name, first_name, gender,
+            dob, height, weight, addr1, addr2, phone1, phone2, email,
+            medical_history, medications, family_hx, allergy, note, attachment
+        ))
 
-	@with_database	
-	def patient_exists(self, cur, patient_key):
-		query = 'SELECT COUNT(*) FROM patient WHERE patient_key=?'
-		count = cur.execute(query, (patient_key,)).fetchone()[0]
-		return bool(count)
+    @with_database    
+    def patient_exists(self, cur, patient_key):
+        query = 'SELECT COUNT(*) FROM patient WHERE patient_key=?'
+        count = cur.execute(query, (patient_key,)).fetchone()[0]
+        return bool(count)
 
-	@with_database
-	def patient_search(self, cur, **kwargs):
-		query = 'SELECT * FROM patient WHERE ' 
-		query += ' AND '.join([ a + ' LIKE ?' for a in kwargs ])
-		values = [ '%'+kwargs[a]+'%' for a in kwargs ]
-		resp = cur.execute(query, values).fetchall()
-		return resp
-	
-	@with_database
-	def patient_search_query(self, cur, search, columns=['first_name', 'last_name', 'phone1', 'phone2', 'email']):
-		if len(search) < 3: search = ''
-		query = 'SELECT * FROM patient WHERE '
-		search = search.split()
-		values = []
-		items = []
-		for item in search:
-			for col in columns:
-				items.append(col + ' LIKE ?')
-				values.append('%' + item + '%')
-		query += ' OR '.join(items)
-		if not len(items): query = 'SELECT * FROM patient'
-		resp = cur.execute(query, values).fetchall()
-		return resp
+    @with_database
+    def patient_search(self, cur, **kwargs):
+        query = 'SELECT * FROM patient WHERE ' 
+        query += ' AND '.join([ a + ' LIKE ?' for a in kwargs ])
+        values = [ '%'+kwargs[a]+'%' for a in kwargs ]
+        resp = cur.execute(query, values).fetchall()
+        return resp
+    
+    @with_database
+    def patient_search_query(self, cur, search, columns=['first_name', 'last_name', 'phone1', 'phone2', 'email']):
+        if len(search) < 3: search = ''
+        query = 'SELECT * FROM patient WHERE '
+        search = search.split()
+        values = []
+        items = []
+        for item in search:
+            for col in columns:
+                items.append(col + ' LIKE ?')
+                values.append('%' + item + '%')
+        query += ' OR '.join(items)
+        if not len(items): query = 'SELECT * FROM patient'
+        resp = cur.execute(query, values).fetchall()
+        return resp
 
-	@with_database
-	def visit_add(self, cur, patient_key, doctor, temperature=None,
-				  heart_rate=None, blood_pressure_h=None, blood_pressure_l=None,
-				  chief_complaint=None, present_illness=None, feedback=None,
-				  exam_appetite=None, exam_digest=None, exam_bm=None,
-				  exam_sleep=None, exam_tongue=None, exam_pulse_l=None,
-				  exam_pulse_r=None, tcm_diag_1=None, tcm_diag_2=None,
-				  tcm_diag_3=None, tcm_diag_4=None, treat_principle=None,
-				  acu_points=None, moxa=None, cupping=None, eacu=None,
-				  arricular=None, condition_treated=None, fee=None, paid=None, 
-				  paid_check_by=None, note=None): 
-		
-		if not self.patient_exists(patient_key):
-			raise PatientNotFound()
-		
-		visit_date = datetime.date.today()
+    @with_database
+    def visit_add(self, cur, patient_key, doctor, temperature=None,
+                  heart_rate=None, blood_pressure_h=None, blood_pressure_l=None,
+                  chief_complaint=None, present_illness=None, feedback=None,
+                  exam_appetite=None, exam_digest=None, exam_bm=None,
+                  exam_sleep=None, exam_tongue=None, exam_pulse_l=None,
+                  exam_pulse_r=None, tcm_diag_1=None, tcm_diag_2=None,
+                  tcm_diag_3=None, tcm_diag_4=None, treat_principle=None,
+                  acu_points=None, moxa=None, cupping=None, eacu=None,
+                  arricular=None, condition_treated=None, fee=None, paid=None, 
+                  paid_check_by=None, note=None): 
+        
+        if not self.patient_exists(patient_key):
+            raise PatientNotFound()
+        
+        visit_date = datetime.date.today()
 
-		query = 'INSERT INTO visit VALUES (%s)' % ('?,' * 32)[:-1]
-		cur.execute(query, (
-			patient_key, doctor, visit_date, temperature, heart_rate,
-			blood_pressure_h, blood_pressure_l, chief_complaint,
-			present_illness, feedback, exam_appetite, exam_digest, exam_bm,
-			exam_sleep, exam_tongue, exam_pulse_l, exam_pulse_r, tcm_diag_1,
-			tcm_diag_2, tcm_diag_3, tcm_diag_4, treat_principle, acu_points,
-			moxa, cupping, eacu, arricular, condition_treated, fee, paid,
-			paid_check_by, note
-		))
-	
-	@with_database
-	def new_visit_add(self, cur, patient_key, doctor, temperature=None,
-					  heart_rate=None, blood_pressure_h=None,
-					  blood_pressure_l=None, chief_complaint=None,
-					  location=None, onset=None, provocation=None,
-					  palliation=None, quality=None, region=None, severity=None,
-					  frequency=None, timing=None, possible_cause=None,
-					  remark=None, present_illness=None, tq_fever=None,
-					  tq_perspiration=None, tq_thirst=None, tq_appetite=None,
-					  tq_digestion=None, tq_taste=None, tq_bm=None,
-					  exam_urine=None, exam_sleep=None, exam_pain=None,
-					  exam_consciousness=None, exam_energy_level=None,
-					  exam_stress_level=None, exam_pulse_l=None,
-					  exam_pulse_r=None, women_menarche=None,
-					  women_menopause=None, women_num_pregnant=None,
-					  women_num_child=None, women_miscarriage=None,
-					  women_leukorrhea=None, women_birth_control=None,
-					  women_menstruation=None, tcm_diag_1=None, tcm_diag_2=None,
-					  tcm_diag_3=None, tcm_diag_4=None, treat_principle=None,
-					  acu_points=None, moxa=None, cupping=None, eacu=None,
-					  auricular=None, condition_treated=None, fee=None,
-					  paid=None, paid_check_by=None, note=None):
+        query = 'INSERT INTO visit VALUES (%s)' % ('?,' * 32)[:-1]
+        cur.execute(query, (
+            patient_key, doctor, visit_date, temperature, heart_rate,
+            blood_pressure_h, blood_pressure_l, chief_complaint,
+            present_illness, feedback, exam_appetite, exam_digest, exam_bm,
+            exam_sleep, exam_tongue, exam_pulse_l, exam_pulse_r, tcm_diag_1,
+            tcm_diag_2, tcm_diag_3, tcm_diag_4, treat_principle, acu_points,
+            moxa, cupping, eacu, arricular, condition_treated, fee, paid,
+            paid_check_by, note
+        ))
+    
+    @with_database
+    def new_visit_add(self, cur, patient_key, doctor, temperature=None,
+                      heart_rate=None, blood_pressure_h=None,
+                      blood_pressure_l=None, chief_complaint=None,
+                      location=None, onset=None, provocation=None,
+                      palliation=None, quality=None, region=None, severity=None,
+                      frequency=None, timing=None, possible_cause=None,
+                      remark=None, present_illness=None, tq_fever=None,
+                      tq_perspiration=None, tq_thirst=None, tq_appetite=None,
+                      tq_digestion=None, tq_taste=None, tq_bm=None,
+                      exam_urine=None, exam_sleep=None, exam_pain=None,
+                      exam_consciousness=None, exam_energy_level=None,
+                      exam_stress_level=None, exam_pulse_l=None,
+                      exam_pulse_r=None, women_menarche=None,
+                      women_menopause=None, women_num_pregnant=None,
+                      women_num_child=None, women_miscarriage=None,
+                      women_leukorrhea=None, women_birth_control=None,
+                      women_menstruation=None, tcm_diag_1=None, tcm_diag_2=None,
+                      tcm_diag_3=None, tcm_diag_4=None, treat_principle=None,
+                      acu_points=None, moxa=None, cupping=None, eacu=None,
+                      auricular=None, condition_treated=None, fee=None,
+                      paid=None, paid_check_by=None, note=None):
 
-		if not self.patient_exists(patient_key):
-			raise PatientNotFound()
-		
-		visit_date = datetime.date.today()
+        if not self.patient_exists(patient_key):
+            raise PatientNotFound()
+        
+        visit_date = datetime.date.today()
 
-		query = 'INSERT INTO visit VALUES (%s)' % ('?,' * 58)[:-1]
-		cur.execute(query, (
-			patient_key, doctor, visit_date, temperature, heart_rate,
-			blood_pressure_h, blood_pressure_l, chief_complaint, location,
-			onset, provocation, palliation, quality, region, severity,
-			frequency, timing, possible_cause, remark, present_illness,
-			tq_fever, tq_perspiration, tq_thirst, tq_appetite, tq_digestion,
-			tq_taste, tq_bm, exam_urine, exam_sleep, exam_pain,
-			exam_consciousness, exam_energy_level, exam_stress_level,
-			exam_pulse_l, exam_pulse_r, women_menarche, women_menopause,
-			women_num_pregnant, women_num_child, women_miscarriage,
-			women_leukorrhea, women_birth_control, women_menstruation,
-			tcm_diag_1, tcm_diag_2, tcm_diag_3, tcm_diag_4, treat_principle,
-			acu_points, moxa, cupping, auricular, condition_treated, fee, paid,
-			paid_check_by, note
-		))
+        query = 'INSERT INTO visit VALUES (%s)' % ('?,' * 58)[:-1]
+        cur.execute(query, (
+            patient_key, doctor, visit_date, temperature, heart_rate,
+            blood_pressure_h, blood_pressure_l, chief_complaint, location,
+            onset, provocation, palliation, quality, region, severity,
+            frequency, timing, possible_cause, remark, present_illness,
+            tq_fever, tq_perspiration, tq_thirst, tq_appetite, tq_digestion,
+            tq_taste, tq_bm, exam_urine, exam_sleep, exam_pain,
+            exam_consciousness, exam_energy_level, exam_stress_level,
+            exam_pulse_l, exam_pulse_r, women_menarche, women_menopause,
+            women_num_pregnant, women_num_child, women_miscarriage,
+            women_leukorrhea, women_birth_control, women_menstruation,
+            tcm_diag_1, tcm_diag_2, tcm_diag_3, tcm_diag_4, treat_principle,
+            acu_points, moxa, cupping, auricular, condition_treated, fee, paid,
+            paid_check_by, note
+        ))
 
 # db = Database('database.db')
 # print(db.patient_search_query(''))
 
 # db.patient_add(
-# 	'Jules', 'Mary', 'f', datetime.date(1956, 3, 14),
-# 	height=65, weight=196, addr1='5677 Autumn Drive',
-# 	email='mjuliee3@gmail.com', phone1=6699937412
+#     'Jules', 'Mary', 'f', datetime.date(1956, 3, 14),
+#     height=65, weight=196, addr1='5677 Autumn Drive',
+#     email='mjuliee3@gmail.com', phone1=6699937412
 # )
 
 
 # # self, cur, patient_key, last_name, first_name, gender, dob,
-# # 					height=None, weight=None, addr1=None, addr2=None,
-# # 					phone1=None, phone2=None, email=None, medical_history=None,
-# # 					medications=None, family_hx=None, allergy=None, note=None,
-# # 					attachment=None):
+# #                     height=None, weight=None, addr1=None, addr2=None,
+# #                     phone1=None, phone2=None, email=None, medical_history=None,
+# #                     medications=None, family_hx=None, allergy=None, note=None,
+# #                     attachment=None):
--- main.py
+++ main.py
@@ -5,121 +5,121 @@ import secrets
 import sys
 
 with open('config.json', 'r') as f:
-	config = json.load(f)
+    config = json.load(f)
 
 app = Flask(__name__)
 db = database.Database('database.db')
 
 if config['debug']:
-	app.secret_key = secrets.token_hex(config['secret_length'])
+    app.secret_key = secrets.token_hex(config['secret_length'])
 else:
-	app.secret_key = 'super_secret_debug_key'
+    app.secret_key = 'super_secret_debug_key'
 
 def require_authentication(func):
-	def wrapper(*args, **kwargs):
-		if session['logged_in']:
-			return func(*args, **kwargs)
-		else:
-			return redirect('/login')
-	wrapper.__name__ = func.__name__
-	return wrapper
+    def wrapper(*args, **kwargs):
+        if session['logged_in']:
+            return func(*args, **kwargs)
+        else:
+            return redirect('/login')
+    wrapper.__name__ = func.__name__
+    return wrapper
 
 @app.before_request
 def before_request():
-	if 'logged_in' not in session:
-		session['logged_in'] = False
-		session['doctor'] = None
+    if 'logged_in' not in session:
+        session['logged_in'] = False
+        session['doctor'] = None
 
 @app.errorhandler(404)
 def page_not_found(error):
-	return render_template('error.html',
-		code=404,
-		message='Page not found',
-		doctor=session['doctor']
-	), 404
+    return render_template('error.html',
+        code=404,
+        message='Page not found',
+        doctor=session['doctor']
+    ), 404
 
 @app.errorhandler(500)
 def page_not_found(error):
-	return render_template('error.html',
-		code=500,
-		message='An internal server error occured',
-		doctor=session['doctor']
-	), 500
+    return render_template('error.html',
+        code=500,
+        message='An internal server error occured',
+        doctor=session['doctor']
+    ), 500
 
 @app.route('/', methods=['GET'])
 def index():
-	return redirect('/login')
+    return redirect('/login')
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-	if request.method == 'GET':
-		if session['logged_in']:
-			return redirect('/home')
-		return render_template('login.html')
-	elif request.method == 'POST':
-		doctor = request.values.get('doctor')
-		password = request.values.get('password')
-		if db.doctor_check(doctor, password):
-			session['logged_in'] = True
-			session['doctor'] = doctor
-			return redirect('/home')
-		else:
-			return redirect('login?err=1')
+    if request.method == 'GET':
+        if session['logged_in']:
+            return redirect('/home')
+        return render_template('login.html')
+    elif request.method == 'POST':
+        doctor = request.values.get('doctor')
+        password = request.values.get('password')
+        if db.doctor_check(doctor, password):
+            session['logged_in'] = True
+            session['doctor'] = doctor
+            return redirect('/home')
+        else:
+            return redirect('login?err=1')
 
 @app.route('/logout', methods=['GET'])
 def logout():
-	session['logged_in'] = False
-	session['doctor'] = None
-	return redirect('/login')
+    session['logged_in'] = False
+    session['doctor'] = None
+    return redirect('/login')
 
 @app.route('/reports')
 @require_authentication
 def reports():
-	return render_template('reports.html', doctor=session['doctor'])
+    return render_template('reports.html', doctor=session['doctor'])
 
 @app.route('/home')
 @require_authentication
 def home():
-	query = request.args.get('query')
-	if not query: query = ''
-	users = db.patient_search_query(query)
-	truncated = False
-	if len(users) > config['max_results']:
-		users = users[:config['max_results']]
-		truncated = True
-	return render_template('home.html',
-		doctor=session['doctor'],
-		query=query,
-		users=users,
-		truncated=truncated,
-		max_results=config['max_results']
-	)
+    query = request.args.get('query')
+    if not query: query = ''
+    users = db.patient_search_query(query)
+    truncated = False
+    if len(users) > config['max_results']:
+        users = users[:config['max_results']]
+        truncated = True
+    return render_template('home.html',
+        doctor=session['doctor'],
+        query=query,
+        users=users,
+        truncated=truncated,
+        max_results=config['max_results']
+    )
 
 @app.route('/patient/<pkey>')
 @require_authentication
 def patient_detail(pkey):
-	results = db.patient_search(patient_key=pkey)
-	if len(results) != 1: abort(500)
-	else: patient = results[0]
-	return render_template('patient/view.html', doctor=session['doctor'], pkey=pkey, patient=patient)
+    results = db.patient_search(patient_key=pkey)
+    if len(results) != 1: abort(500)
+    else: patient = results[0]
+    return render_template('patient/view.html', doctor=session['doctor'], pkey=pkey, patient=patient)
 
 @app.route('/patient/new')
 @require_authentication
 def patient_new():
-	return render_template('patient/new.html', doctor=session['doctor'])
+    return render_template('patient/new.html', doctor=session['doctor'])
 
 @app.route('/patient/<pkey>/new_visit')
 @require_authentication
 def patient_new_visit(pkey):
-	return render_template('patient/new_visit.html', doctor=session['doctor'], pkey=pkey)
+    return render_template('patient/new_visit.html', doctor=session['doctor'], pkey=pkey)
 
 @app.route('/patient/<pkey>/return_visit')
 @require_authentication
 def patient_return_visit(pkey):
-	return render_template('patient/return_visit.html', doctor=session['doctor'], pkey=pkey)
+    return render_template('patient/return_visit.html', doctor=session['doctor'], pkey=pkey)
 
 app.run(
-	debug=config['debug'],
-	host=config['ip'],
-	port=config['port']
+    debug=config['debug'],
+    host=config['ip'],
+    port=config['port']
 )
