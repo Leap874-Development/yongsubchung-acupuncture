d6e0df6f7afcea2a043e8fc3add0a6285b152a00
William Gardner <wg4568@gmail.com>
2019-11-10 10:05:01 -0800

    new schema

--- documents/schema.sql
+++ documents/schema.sql
@@ -1,6 +1,6 @@
 create table doctors (
-       username TEXT NOT NULL,
-       password TEXT NOT NULL		-- Auto: werkzeug hash
+	username TEXT NOT NULL,
+	password TEXT NOT NULL		-- Auto: werkzeug hash
 );
 
 --  patient_key format : LYYMMDD[S+]
@@ -15,7 +15,7 @@ create table doctors (
 --
 create table patient (
 	create_date		DATE NOT NULL,			-- Auto
-	suffix	    	INTEGER NOT NULL,		-- Auto
+	suffix			INTEGER NOT NULL,		-- Auto
 	patient_key		TEXT NOT NULL UNIQUE,	-- Auto
 	last_name		TEXT NOT NULL,
 	first_name		TEXT NOT NULL,
@@ -23,90 +23,90 @@ create table patient (
 	dob				DATE NOT NULL,
 	height			TEXT, 
 	weight			TEXT,
-	addr1   		TEXT,
-	addr2   		TEXT,
-	phone1  		TEXT,
-	phone2  		TEXT,
-	email   		TEXT,
+	addr1			TEXT,
+	addr2			TEXT,
+	phone1			TEXT,
+	phone2			TEXT,
+	email			TEXT,
 	medical_history	TEXT,
 	medications		TEXT,
 	family_hx		TEXT,
 	allergy			TEXT,
-	note            TEXT,
-	attachment	  	TEXT       -- URL for s3-object
+	note			TEXT,
+	attachment		TEXT       -- URL for s3-object
 );
 
 create table visit (
 	-- BOTH TYPES
-	visit_fkey 			TEXT NOT NULL UNIQUE,
-	new_visit 			BOOLEAN NOT NULL,
-	patient_fkey 		TEXT NOT NULL,
-	doctor 				TEXT NOT NULL,
-	visit_date 			DATE NOT NULL,	-- Auto
-	temperature 		REAL,
-	heart_rate 			REAL,
-	blood_pressure_h 	INTEGER,
-	blood_pressure_l 	INTEGER,
-	chief_complaint 	TEXT,
-	present_illness 	TEXT,
-	exam_pulse_l 		TEXT,
-	exam_pulse_r 		TEXT,
-	exam_sleep 			TEXT,
-	exam_tongue 		TEXT,
-	tcm_diag_1 			TEXT,
-	tcm_diag_2 			TEXT,
-	tcm_diag_3 			TEXT,
-	tcm_diag_4 			TEXT,
-	treat_principle 	TEXT,
-	acu_points 			TEXT,
-	moxa 				BOOLEAN,
-	cupping 			BOOLEAN,
-	eacu 				BOOLEAN,
-	auricular 			TEXT,
-	condition_treated 	TEXT,
-	fee 				REAL,
-	paid 				BOOLEAN,
-	paid_check_by 		TEXT, 		-- Doctor(username) who received the fee.
-	note 				TEXT
+	visit_fkey			TEXT NOT NULL UNIQUE,
+	new_visit			BOOLEAN NOT NULL,
+	patient_fkey		TEXT NOT NULL,
+	doctor				TEXT NOT NULL,
+	visit_date			DATE NOT NULL,	-- Auto
+	temperature			REAL,
+	heart_rate			REAL,
+	blood_pressure_h	INTEGER,
+	blood_pressure_l	INTEGER,
+	chief_complaint		TEXT,
+	present_illness		TEXT,
+	exam_pulse_l		TEXT,
+	exam_pulse_r		TEXT,
+	exam_sleep			TEXT,
+	exam_tongue			TEXT,
+	tcm_diag_1			TEXT,
+	tcm_diag_2			TEXT,
+	tcm_diag_3			TEXT,
+	tcm_diag_4			TEXT,
+	treat_principle		TEXT,
+	acu_points			TEXT,
+	moxa				BOOLEAN,
+	cupping				BOOLEAN,
+	eacu				BOOLEAN,
+	auricular			TEXT,
+	condition_treated	TEXT,
+	fee					REAL,
+	paid				BOOLEAN,
+	paid_check_by		TEXT, 		-- Doctor(username) who received the fee.
+	note				TEXT
 
 	-- VISIT ONLY
 	feedback			TEXT,
-	exam_appetite 		TEXT,
-	exam_digest 		TEXT,
-	exam_bm 			TEXT,
+	exam_appetite		TEXT,
+	exam_digest			TEXT,
+	exam_bm				TEXT,
 
 	-- NEW VISIT ONLY
-	location 			TEXT,
-	onset 				TEXT,
-	provocation 		TEXT,
-	palliation 			TEXT,
-	quality 			TEXT,
-	region 				TEXT,
-	severity 			TEXT,
-	frequency 			TEXT,
+	location			TEXT,
+	onset				TEXT,
+	provocation			TEXT,
+	palliation			TEXT,
+	quality				TEXT,
+	region				TEXT,
+	severity			TEXT,
+	frequency			TEXT,
 	timing 				TEXT,
-	possible_cause 		TEXT,
-	remark 				TEXT,
-	tq_fever 			TEXT,
-	tq_perspiration 	TEXT,
-	tq_thirst 			TEXT,
-	tq_appetite 		TEXT,
-	tq_digestion 		TEXT,
-	tq_taste 			TEXT,
-	tq_bm 				TEXT,
-	exam_urine 			TEXT,
+	possible_cause		TEXT,
+	remark				TEXT,
+	tq_fever			TEXT,
+	tq_perspiration		TEXT,
+	tq_thirst			TEXT,
+	tq_appetite			TEXT,
+	tq_digestion		TEXT,
+	tq_taste			TEXT,
+	tq_bm				TEXT,
+	exam_urine			TEXT,
 	exam_pain 			TEXT,
-	exam_consciousness 	TEXT,
-	exam_energy_level 	INTEGER,
-	exam_stress_level 	INTEGER,
-	women_menarche 		TEXT,
-	women_menopause 	TEXT,
+	exam_consciousness	TEXT,
+	exam_energy_level	INTEGER,
+	exam_stress_level	INTEGER,
+	women_menarche		TEXT,
+	women_menopause		TEXT,
 	women_num_pregnant	TEXT,
 	women_num_child		TEXT,
-	women_miscarriage 	TEXT,
-	women_leukorrhea 	TEXT,
-	women_birth_control TEXT,
-	women_menstruation 	TEXT
+	women_miscarriage	TEXT,
+	women_leukorrhea	TEXT,
+	women_birth_control	TEXT,
+	women_menstruation	TEXT
 );
 
 
